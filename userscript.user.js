4859c846d98f97d38e0b7e24c1d4f0ceab33490f
RealMVC <39596187+RealMVC@users.noreply.github.com>
2022-02-12 17:42:22 +0000

    Updated userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -686,7 +686,7 @@
                         var gamemap = document.getElementById("gamemap");
                         previewElement = document.createElement("div");
                         previewElement.innerHTML = html;
-                        previewElement.setAttribute("style", "scale: ".concat(localStorage.getItem("scale"), "; top: 39.7%; position: relative; left: -0.3%;"));
+                        previewElement.setAttribute("style", "scale: ".concat(localStorage.getItem("scale"), "; height: 0.5%"));
                         var mapBackground = Object.values(previewElement.childNodes).find(function (c) { return c.id === "map-background"; });
                         mapBackground.src = mapBackground.src += "?" + Date.now();
                         gamemap.style.display = "none";
